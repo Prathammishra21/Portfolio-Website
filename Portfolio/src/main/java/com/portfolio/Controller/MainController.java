dfd5d36124b5c80b02e2ac9e1e30e09f5e12f432
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-13 21:12:09 +0530

    contact page added

--- Portfolio/src/main/java/com/portfolio/Controller/MainController.java
+++ Portfolio/src/main/java/com/portfolio/Controller/MainController.java
@@ -17,54 +17,54 @@ public class MainController {
 
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "/Html_Page/home";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "about";
+        return "/Html_Page/about";
     }
 
     @GetMapping("/services")
     public String services() {
-        return "services";
+        return "/Html_Page/services";
     }
 
     @GetMapping("/contact")
     public String contact() {
-        return "contact";
+        return "/Html_Page/contact";
     }
 
     @GetMapping("/blog")
     public String getAllBlogPosts(Model model) {
         List<BlogPost> blogPosts = blogPostService.getAllBlogPosts();
         model.addAttribute("blogPosts", blogPosts);
-        return "blog";
+        return "/Html_Page/blog";
     }
 
     @GetMapping("/blog/{id}")
     public String getBlogPostById(@PathVariable Long id, Model model) {
         Optional<BlogPost> blogPost = blogPostService.getBlogPostById(id);
         blogPost.ifPresent(post -> model.addAttribute("blogPost", post));
-        return "blog";
+        return "/Html_Page/blog";
     }
 
     @PostMapping("/blog")
     public String createBlogPost(@ModelAttribute("blogPost") BlogPost blogPost) {
         blogPostService.createOrUpdateBlogPost(blogPost);
-        return "redirect:/blog";
+        return "redirect:/Html_Page/blog";
     }
 
     @PutMapping("/blog/{id}")
     public String updateBlogPost(@PathVariable Long id, @ModelAttribute("blogPost") BlogPost blogPost) {
         blogPost.setId(id);
         blogPostService.createOrUpdateBlogPost(blogPost);
-        return "redirect:/blog";
+        return "redirect:/Html_Page/blog";
     }
 
     @DeleteMapping("/blog/{id}")
     public String deleteBlogPost(@PathVariable Long id) {
         blogPostService.deleteBlogPost(id);
-        return "redirect:/blog";
+        return "redirect:/Html_Page/blog";
     }
 }
